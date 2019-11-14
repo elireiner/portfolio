ce3f6486ab3d67b88b1d4ba5bdf834c8d2485d25
Eli Reiners <goelyukim@gmail.com>
2019-11-14 14:18:08 -0500

    some updates

---
+++ README.md
@@ -0,0 +1,5 @@
+    <ul>
+            <li><a class="eli-reiner" href="">Eli Reiner</a></li>
+            <li><a class="contact" href="">Contact</a></li>
+            <li><a class="portfolio" href="">Portfolio</a></li>
+        </ul>
---
+++ index.js
@@ -0,0 +1,34 @@
+function hideUnwanted(code1, code2) {
+    $(code1).hide();
+    $(code2).hide();
+}
+
+function renderEli() {
+    $('nav').on('click', '.eli-reiner', function () {
+        hideUnwanted('#contact', '#portfolio');
+        $('#about').show()
+    })
+}
+
+function renderContact() {
+    $('nav').on('click', '.contact', function () {
+        hideUnwanted('#about', '#portfolio');
+        $('#contact').show();
+    })
+}
+
+function renderPortfolio() {
+    $('nav').on('click', '.portfolio', function () {
+        hideUnwanted('#contact', '#about');
+        $('#portfolio').show();
+    })
+}
+
+function launchPortfolio() {
+    hideUnwanted('#contact', '#portfolio');
+    renderEli();
+    renderContact();
+    renderPortfolio();
+}
+
+$(launchPortfolio);
